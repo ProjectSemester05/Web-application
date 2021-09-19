f69973105a8091148576bafb21b2bd50e208441a
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2021-09-19 07:47:00 +0530

    catalogue page - change 2.0

--- src/pages/catalogues.jsx
+++ src/pages/catalogues.jsx
@@ -1,15 +1,25 @@
 import { React } from "react";
-import { Box, Text, Flex } from "@chakra-ui/react";
+import ReactDOM from 'react-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AddItemPopup from "../components/additem_func";
+import { Box, Text, Flex, Button,Image } from "@chakra-ui/react";
 import "@fontsource/montserrat";
 import { Heading } from "@chakra-ui/react"
+import { Divider } from "@chakra-ui/react"
 import Footer from "../components/footer";
 import Header from "../components/header";
+import InitialFocus from "../components/catalogue_func";
 import CatalogueCard from "../components/cards/catalogue";
 import ItemBox from "../components/cards/itembox";
 import { CheckCircleIcon } from '@chakra-ui/icons';
 import "../style/catalogues.css";
+import {
+  Table,Thead,Tbody,Tfoot,Tr,Th,Td,TableCaption,
+} from "@chakra-ui/react";
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
 
 const CataloguePage = () => {
+
   return (
     <Flex flexDirection="column" position="relative">
       <Flex>
@@ -52,54 +62,86 @@ const CataloguePage = () => {
       <Box backgroundColor="#000000" mb="6" border="1px solid #000000" />
 
       <Text fontSize="22px" as="" ml={20} mb={5}><CheckCircleIcon w={5} h={5} mr={5} mb={1}/>Items</Text>
+      <AddItemPopup />
 
-      <ItemBox
-        name="Kettle"
-        description = "Newly bought electric kettle"
-        img="/assets/images/paint.png"
-        reminder = "21/09/2021"
-      />
-      <ItemBox
-        name="Mug"
-        description = "Mug with the red handle"
-        img="/assets/images/paint.png"
-        reminder = "21/09/2021"
-      />
-      <ItemBox
-        name="Beater"
-        description = "No description included"
-        img="/assets/images/paint.png"
-        reminder = "none"
-      />
-      <ItemBox
-        name="Fork"
-        description = "No description included"
-        img="/assets/images/paint.png"
-        reminder = "none"
-      />
-      <ItemBox
-        name="Tea Bag"
-        description = "No description included"
-        img="/assets/images/paint.png"
-        reminder = "none"
-      />
-      <ItemBox
-        name="Grinder"
-        description = "No description included"
-        img="/assets/images/paint.png"
-        reminder = "none"
-      />
-      <ItemBox
-        name="Blender"
-        description = "No description included"
-        img="/assets/images/paint.png"
-        reminder = "none"
-      />    
+      <Table variant="simple"
+            mt="0"
+            ml = {100}
+            mt ={2}
+            width="1350px"
+            maxH="80px"
+            flexDirection="column"
+            mb="0"
+            border-radius="24px"
+            backgroundColor="white"
+            borderRadius="24px"
+            border="1px solid #CCC9C9"
+            boxSizing="border-box">
+        <TableCaption></TableCaption>
+          <Thead>
+          <Tr>
+            <Th>Image</Th>
+            <Th>Item Name</Th>
+            <Th>Description</Th>
+            <Th>Reminder</Th>
+            <Th textAlign="center">Edit/Delete</Th>
+          </Tr>
+          </Thead>
+
+          <Tbody>
+            <ItemBox
+              name="Kettle"
+              description = "Newly bought electric kettle"
+              img="/assets/images/paint.png"
+              reminder = "2021-09-21"
+            />
+            <ItemBox
+              name="Mug"
+              description = "Mug with the red handle"
+              img="/assets/images/paint.png"
+              reminder = "21/09/2021"
+            />
+            <ItemBox
+              name="Beater"
+              description = "No description included"
+              img="/assets/images/paint.png"
+              reminder = "none"
+            />
+            <ItemBox
+              name="Fork"
+              description = "No description included"
+              img="/assets/images/paint.png"
+              reminder = "none"
+            />
+            <ItemBox
+              name="Tea Bag"
+              description = "No description included"
+              img="/assets/images/paint.png"
+              reminder = "none"
+            />
+            <ItemBox
+              name="Grinder"
+              description = "No description included"
+              img="/assets/images/paint.png"
+              reminder = "none"
+            />
+            <ItemBox
+              name="Blender"
+              description = "No description included"
+              img="/assets/images/paint.png"
+              reminder = "none"
+            />    
+          </Tbody>
+      </Table>
+
+      
       <Flex mt="auto">
-        <Footer minHeight="20px" position="static" bottom="0" />
+        <Footer minHeight="20px"  bottom="0" />
       </Flex>
     </Flex>
   );
 };
+
+
 
 export default CataloguePage;
